cc09bc96d28ed10c9e2e592fb36cd1740a877302
losstie <losstie@outlook.com>
2021-03-20 22:23:12 +0800

    fix

--- vue-netease-music/src/router/index.js
+++ vue-netease-music/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
 import Podcast from "@/views/Podcast.vue";
 import User from "@/views/User.vue";
 import Ksong from "@/views/Ksong.vue";
